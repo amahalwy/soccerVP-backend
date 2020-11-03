908d96ff5a8626a69a45872dfbb5be31c7c48ac8
Ahmed El Mahallawy <amahalwy@gmail.com>
2020-11-03 01:14:17 -0500

    Change seeds load

--- app/views/events/_event.json.jbuilder
+++ app/views/events/_event.json.jbuilder
@@ -1,1 +1,2 @@
-json.extract! event, :id, :created_at, :updated_at, :location
+json.extract! event, :id, :created_at, :updated_at, :location, :starts_at, :ends_at,
+:max_participants, :cost_per_participant, :payment_link, :payment_type #,#event.user
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Event.destroy_all
+User.destroy_all
+
 user = User.create(
   phone_number: '14157696068',
   first_name: 'Mohamed',
@@ -18,6 +21,16 @@ user.events.create(
   ends_at: DateTime.now + 2.hour,
   location: 'San Francisco',
   max_participants: 12,
+  payment_type: :paypal,
+  payment_link: 'https://paypal.com',
+  cost_per_participant: 5,
+)
+
+user.events.create(
+  starts_at: DateTime.now + 1.hour,
+  ends_at: DateTime.now + 2.hour,
+  location: 'San Diego',
+  max_participants: 24,
   payment_type: :paypal,
   payment_link: 'https://paypal.com',
   cost_per_participant: 5,
