b99fc1896c2ebd0ce2f167f7943e45009e47bab2
Ahmed El Mahallawy <amahalwy@gmail.com>
2020-11-06 21:54:38 -0500

    Test for twilio

--- app/services/twilio_client.rb
+++ app/services/twilio_client.rb
@@ -10,8 +10,8 @@ module TwilioClient
 
   def self.client
     debugger
-    account_sid = Rails.application.credentials.twilio_account_sid
-    auth_token = Rails.application.credentials.twilio_auth_token
+    account_sid = Rails.env.production ? Rails.application.credentials.twilio_account_sid : ENV["TWILIO_ACCOUNT_SID"]
+    auth_token = Rails.env.production ? Rails.application.credentials.twilio_auth_token : ENV["TWILIO_AUTH_TOKEN"]
 
     Twilio::REST::Client.new account_sid, auth_token
   end
