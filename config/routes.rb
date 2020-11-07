5b5113488a2235177c93ceab0c9c6d04d0b5493e
Ahmed El Mahallawy <amahalwy@gmail.com>
2020-11-06 20:46:00 -0500

    Edit routes responses

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :events
-  resources :users
-  resources :sessions
+  resources :events, defaults: {format: :json}
+  resources :users, defaults: {format: :json}
+  resources :sessions, defaults: {format: :json}
 
   # get '/current_user', to: 'application#current_user', as: :current_user
   
