5101ca8b5873a8aa7b13dfef246c318fe34f781c
Ahmed El Mahallawy <amahalwy@gmail.com>
2020-11-06 22:08:01 -0500

    Change for twilio info

--- app/services/twilio_client.rb
+++ app/services/twilio_client.rb
@@ -9,8 +9,8 @@ module TwilioClient
   AHMED_PHONE_NUMBER = "+14165625168".freeze
 
   def self.client
-    account_sid = Rails.env.production ? Rails.application.credentials.twilio_account_sid : ENV["TWILIO_ACCOUNT_SID"]
-    auth_token = Rails.env.production ? Rails.application.credentials.twilio_auth_token : ENV["TWILIO_AUTH_TOKEN"]
+    account_sid = Rails.env["development"] ? Rails.application.credentials.twilio_account_sid : ENV["TWILIO_ACCOUNT_SID"]
+    auth_token = Rails.env["development"] ? Rails.application.credentials.twilio_auth_token : ENV["TWILIO_AUTH_TOKEN"]
 
     Twilio::REST::Client.new account_sid, auth_token
   end
